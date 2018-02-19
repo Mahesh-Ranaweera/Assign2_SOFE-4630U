c7baea188e8eb5f48d6fabc5ca3d71d1d95a91df
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-02-19 00:01:16 -0500

    added db create code

--- app/db.js
+++ app/db.js
@@ -1,3 +1,67 @@
 /**
  * Handles database connections
  */
+
+var connection = null;
+var r = require('rethinkdbdash')({
+    port: 28015,
+    host: 'localhost'
+}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+});
+
+/**DB tables */
+var dbname = "app_db";
+var tbusers = "users";
+
+/**Setting up the app tables */
+r.dbList().contains(dbname)
+    .do(function(dbExists) {
+        return r.branch(
+            dbExists, {
+                dbs_created: 0
+            },
+            r.dbCreate(dbname)
+        );
+    }).run();
+
+/**Create table users */
+r.db(dbname).tableList().contains(tbusers)
+    .do(function(dbTableExists) {
+        return r.branch(
+            dbTableExists, {
+                tables_created: 0
+            },
+            r.db(dbname).tableCreate(tbusers, {
+                primaryKey: 'email'
+            }));
+    }).run();
+
+
+/**USER signup */
+var addUSER = function(data, callback) {
+    //check if email exists
+    r.db(dbname).table(tbusers).get(data.email).run()
+        .then(function(response) {
+            if (response == null) {
+                console.log('user not exists');
+
+                //insert the new user
+                r.db(dbname).table(tbusers).insert({
+                    'email': data.email,
+                    'fname': data.fname,
+                    'lname': data.lname,
+                    'verified': false,
+                    'passw': hash
+                }).run();
+
+                callback(1);
+            } else {
+                console.log('user found');
+                callback(-1);
+            }
+        }).catch(function(err) {
+            callback(0);
+        });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var dbconn = require('../app/db');
+var session = require('express-session');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
